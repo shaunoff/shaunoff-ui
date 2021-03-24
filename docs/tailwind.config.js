0fa79ba01d8812f1673c34c9b943b5b7b0c781b3
shaun Hutch <shaun@promoboxx.com>
2021-03-23 22:02:26 -0400

    purge

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -3,8 +3,7 @@ const themeWrapper = require('@shaunoff-ui/components/config')
 module.exports = themeWrapper({
   purge: {
     // enabled: true,
-    // content: ['./**/*.html', './**/*.ts', './**/*.tsx', '../docs/**/*.html', '../docs/**/*.ts', '../docs/**/*.tsx'],
-    content: [],
+    content: ['./**/*.html', './**/*.ts', './**/*.tsx', '../docs/**/*.html', '../docs/**/*.ts', '../docs/**/*.tsx'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
